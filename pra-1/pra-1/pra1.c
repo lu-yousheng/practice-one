d32fe51f282a5cf0e6c8b46a69e2c45c1e7c1035
lu-yousheng <2269975236@qq.com>
2023-01-20 21:42:53 +0800

    ok

--- pra-1/pra-1/pra1.c
+++ pra-1/pra-1/pra1.c
@@ -1,6 +1,54 @@
 #include<stdio.h>
+
 int main()
 {
-	printf("hello world!");
+	int a,b=0;
+	/*extern int global;*/
+	scanf("%d%d", &a,&b);
+	int sum = a + b;
+	printf("sum=%d\n", sum);
+	/*printf("%d\n", global);
+	abc();*/
 	return 0;
 }
+//int abc()
+//{
+//	int abandon = 12;
+//    scanf("%d\n", &abandon);
+//	printf("%d\n", abandon);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b;
+//	scanf_s("%d%d\", &a, &b);
+//	int sum = a + b;
+//	printf("sum=%d\n", sum);
+//	return 0;
+//}
+#include<stdio.h>
+//int main()
+//{
+//	enum Sex
+//	{
+//		male,female
+//	};
+//	enum Sex s = male;
+//	printf("%s\n", s);
+//	return 0;
+//}
+//#include<stdio.h>
+//#include<string.h>
+//int main()
+//{
+//	char arr1[] = "abc";
+//	char arr2[] = { 'a','b','c'};
+//	printf("%s\n", arr1);
+//	printf("%s\n", arr2);
+//	int a = strlen(arr1);
+//	printf("%d\n", a);
+//	printf("%d\n", strlen(arr2));
+//
+//	return 0;
+//}
---
+++ pra_two/pra_two/pra2.c
@@ -0,0 +1,127 @@
+//#include<stdio.h>
+//int main()
+//{
+//	char ch[]="yes or no";
+//	printf("你好好学习了吗？");
+//	scanf_s("你的选择是%s", ch);
+//	if (ch=="yes")
+//	{
+//		printf("走上人生巅峰");
+//	}
+//	else
+//	{
+//		printf("回家挑大粪");
+//	}
+//	return 0;
+//}
+#include<stdio.h>
+//int main()
+//{
+//	int line = 0;
+//	while (line<=20000)
+//	{
+//		printf("数星星%d\n", line);
+//		line++;
+//	}
+//		printf("可以停止了");
+//	
+//	return 0;
+//}
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	char ch[] = { 'a','b','c' };
+//	int i = 0;
+//	while (i < 10)
+//	{
+//		printf("%c ", ch[i]);
+//		i++;
+//	}
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 9 % 2;
+//	float b = 9.0 / 2;
+//	printf("%d\n", a);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int arr[10];
+//	printf("%d", sizeof(arr[1]));
+//	return 0;
+//}
+#include<stdio.h>
+//int main()
+//{
+//	int a = 5;
+//	printf("%d", ~a);
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 3;
+//	//int b = ++a;
+//	int b = a++;
+//	printf("%d\n", a);
+//	printf("%d", b);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 1;
+//	int b = 3;
+//	int max = 0;
+//	if (a > b)
+//	{
+//		max = a;
+//	}
+//	else
+//	{
+//		max = b;
+//	}
+//	max = a > b ? a : b;
+//	printf("%d", max);
+//
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int sw = 0;
+//	while (sw <= 100)
+//	{
+//		printf("%d ", sw);
+//		sw++;
+//	}
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int sum=0;
+//	while (i<=10000)
+//	{
+//		sum = sum + i;
+//		i++;
+//	}
+//	printf("%d", sum);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	while (i < 5)
+//	{
+//         printf("%c\n", '*');
+//	}
+//	
+//	return 0;
+//}
+
+
