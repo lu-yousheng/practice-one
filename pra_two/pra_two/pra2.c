47655545fc9aec1462a9a176de729f4b294811ec
lu-yousheng <2269975236@qq.com>
2023-01-28 18:26:11 +0800

    ok

--- pra_two/pra_two/pra2.c
+++ pra_two/pra_two/pra2.c
@@ -54,7 +54,7 @@
 //	printf("%d", sizeof(arr[1]));
 //	return 0;
 //}
-#include<stdio.h>
+//#include<stdio.h>
 //int main()
 //{
 //	int a = 5;
@@ -121,6 +121,126 @@
 //         printf("%c\n", '*');
 //	}
 //	
+//	return 0;
+//}
+//#include<stdio.h>
+//typedef unsigned int u_int;
+//int main()
+//{
+//	unsigned int num = -100;
+//	printf("%d", num);
+//	return 0;
+//}
+//#define add(x,y) ((x)+(y))
+//int main()
+//{
+//	printf("%d", 5*add(1+2*5, 2*4+1));
+//	return 0;
+//}
+//void ab()
+//{
+//	int i = 0;
+//	while (i<20)
+//	{
+//		printf("%c", '*');
+//		i++;
+//
+//	}
+//	
+//}
+//int main()
+//{
+//	int i = 0;
+//	while (i<5)
+//	{
+//		ab();
+//		printf(" ");
+//		i++;
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 5;
+//	printf("通过&的方式找到的a的地址：%p\n", &a);
+//	int* pa = &a;
+//	printf("通过指针变量找到的a的地址：%p\n", pa);
+//	*pa = 2;
+//	printf("%d\n", a);
+//	printf("%p", pa);
+//	return 0;
+//}
+//int main()
+//{
+//	printf("%d\n", sizeof(char*));
+//	printf("%d\n", sizeof(short*));
+//	printf("%d\n", sizeof(int*));
+//	printf("%d\n", sizeof(long*));
+//	printf("%d\n", sizeof(long long*));
+//	printf("%d\n", sizeof(float*));
+//	return 0;
+//}
+//int add(int x, int y)
+//{
+//	int sum = x + y;
+//	return sum;
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf_s("%d %d", &a, &b);
+//	printf("%d", add(a,b));
+//	return 0;
+//}
+//int max(int x,int y)
+//{
+//	int max = 0;
+//	if (x > y)
+//	{
+//		max = x;
+//	}
+//	else
+//	{
+//		max = y;
+//	}
+//	return max;
+//}
+//int max(int x,int y)
+//{
+//	if (x > y)
+//	{
+//		return x;
+//	}
+//	else
+//	{
+//		return y;
+//	}
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf_s("%d %d", &a, &b);
+//	printf("%d", max(a, b));
+//	return 0;
+//}
+//int sum(int a)
+//{
+//	int c = 0;
+//	static int b = 3;
+//	c += 1;
+//	b += 2;
+//	return(a + b + c);
+//}
+//int main()
+//{
+//	int i;
+//	int a = 2;
+//	for (i = 0;i < 5;i++)
+//	{
+//		printf("%d,", sum(a));
+//	}
 //	return 0;
 //}
 
