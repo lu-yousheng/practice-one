7f04888f5eb70751dde0f7d022c8d501ec8946c2
lu-yousheng <2269975236@qq.com>
2023-02-04 23:39:46 +0800

    666

--- pra-four/pra-four/pra4.c
+++ pra-four/pra-four/pra4.c
@@ -18,7 +18,7 @@
 //#include<stdio.h>
 //int my_strlen(char* a)
 //{
-//	if (*a != 0)
+//	if (*a != '\0')
 //	{
 //		return 1 + my_strlen(++a);
 //	}
@@ -109,18 +109,24 @@
 //	printf("总共：%d", count);
 //	return 0;
 //}
-#include<stdio.h>
-int main()
-{
-	float i = 0;
-	float sum = 0;
-	for (i =1;i <= 100;i++)
-	{
-		sum += 1 / i;
-	}
-	printf("%lf", sum);
-	return 0;
-}
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	float sum = 0;
+//	int flag = 1;
+//	for (i =1;i <= 100;i++)
+//	{
+//		//if (i % 2 == 0)
+//		//	sum -= 1.0 / i;
+//		//else
+//		//	sum += 1.0 / i;
+//		sum += flag * 1.0 / i;
+//		flag = -flag;
+//	}
+//	printf("%lf", sum);
+//	return 0;
+//}
 //#include<stdio.h>
 //int main()
 //{
@@ -133,3 +139,232 @@ int main()
 //	printf("%d",arr);
 //	return 0;
 //}
+//#include<stdio.h>
+//int main()
+//{
+//	int arr[10] = { 99,2,3,4,5,6,7,8,9,10 };
+//	int max = 0;
+//	int i = 0;
+//	int sa = sizeof(arr)/sizeof(arr[0]);
+//	for (i = 0;i <= sa - 1;i++)
+//	{
+//		if (arr[i] > arr[i + 1])
+//		{
+//			if (arr[i] > max)
+//				max = arr[i];
+//		}
+//			else
+//			{
+//				if (arr[i] > max)
+//					max = arr[i + 1];
+//			}
+//	}
+//	printf("%d", max);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int arr[10] = { 1,20,3,4,5,6,7,8,9,10 };
+//	int max = arr[0];
+//	int i = 1;
+//	for (i = 1;i <= 10;i++)
+//	{
+//		if (arr[i] > max)
+//			max = arr[i];
+//	}
+//	printf("%d", max);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1;i <= 9;i++)
+//	{
+//		int j = 1;
+//		//for (j = i;j <= 9;j++)
+//		//{
+//		//	int ret = i * j;
+//		//	printf("%d×%d=%d ", i, j, ret);
+//		//}
+//		for (j = 1;j <= i;j++)
+//		{
+//			printf("%d×%d=%-2d ", j, i, i*j);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+//实现一个函数，打印乘法口诀表，口诀表的行数和列数自己指定
+//如输入9，输入9*9口诀表，输出12，输出12*12的乘法口诀表。
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int n = 0;
+//	scanf("%d", &n);
+//	for (i = 1;i <= n;i++)
+//	{
+//		int j = 0;
+//		for (j = 1;j <= i;j++)
+//		{
+//			printf("%d×%d=%-3d ", j, i, i * j);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+//
+//void print(int a)
+//{
+//	if (a > 9)
+//	{
+//
+//		printf("%d", a % 100);
+//	}
+//	else
+//}
+//int main()
+//{
+//	int  a = 0;
+//	scanf("%d", &a);
+//	printf("%d ", print(a));
+//	return 0;
+//}
+//编写一个函数reverse_string(char* string)递归实现
+//将参数字符串中的字符反向排列，不是逆序打印
+//不能使用c函数库中的字符串操作函数
+//用迭代实现
+#include<stdio.h>
+//void reverse_string(char* string)
+//{
+//	int left = 0;
+//	int right = strlen(string) - 1;
+//	int tmp = 0;
+//	if (left != right)
+//	{
+//		reverse_string(string++);
+//	}
+//	tmp = string[left];
+//	string[left] = string[right];
+//	string[right] = tmp;
+//}
+//int main()
+//{
+//	char arr[100] = { "" };
+//	scanf("%s",arr);
+//	
+//	reverse_string(arr);
+//	/*while (left != right)
+//	{
+//		tmp = arr[left];
+//		arr[left] = arr[right];
+//		arr[right] = tmp;
+//		left++;
+//		right--;
+//	}*/
+//	printf("%s", arr);
+//	return 0;
+//}
+//用函数实现
+//int my_strlen(char* strl)
+//{
+//	int count = 0;
+//	while(*strl != '\0')
+//	{
+//		count++;
+//		strl++;
+//	}
+//	return count;
+//}
+//void reverse_string(char* str)
+//{
+//	char tmp = *str;
+//	int len = my_strlen(str);
+//	*str = *(str + len - 1);
+//	*(str + len - 1) = '\0';
+//	if (my_strlen(str+1) >= 2)
+//	{
+//		reverse_string(str + 1);
+//	}
+//	*(str + len - 1) = tmp;
+//
+//	//int left = 0;
+//	//int right = my_strlen(str)-1;
+//	//char tmp = 0;
+//	//while (left < right)
+//	//{
+//	//	tmp = str[left];
+//	//	str[left] = str[right];
+//	//	str[right] = tmp;
+//	//	left++;
+//	//	right--;
+//	//}
+//}
+//int main()
+//{
+//	char arr[] = "abcdef";
+//    reverse_string(arr);
+//	printf("%s", arr);
+//	return 0;
+//}
+//int DigitSum(int x)
+//{
+//	int sum = 0;
+//	if (x > 9)
+//	{	
+//		while (x>9)
+//		{
+//			sum += (x%10);
+//			x = x/10;
+//		}
+//		return sum+x;
+//	}
+//	else
+//		return x;
+//}
+//int DigitSum(int x)
+//{
+//	if (x > 9)
+//	{
+//		return x % 10 + DigitSum(x / 10);
+//	}
+//	else
+//		return x;
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	printf("%d", DigitSum(n));
+//	return 0;
+//}
+//int power(int x,int y)
+//{
+//	int i = 0;
+//	int pw = 1;
+//	for (i = 1;i <= y;i++)
+//	{
+//		pw *= x;
+//	}
+//	return pw;
+//}
+//int power(int x, int y)
+//{
+//	if (y > 1)
+//	{
+//		return x * power(x, y - 1);
+//	}
+//	else
+//		return x;
+//}
+//int main()
+//{
+//	int n = 0;
+//	int k = 0;
+//	scanf("%d %d", &n,&k);
+//	printf("%d", power(n,k));
+//	return 0;
+//}
+//递归完成斐波那契数列
