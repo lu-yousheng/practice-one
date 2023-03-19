771da24ca074364062f38d14c4264466d304cdef
lu-yousheng <2269975236@qq.com>
2023-03-19 23:31:14 +0800

    7777

---
+++ pra-seven/pra-seven/pra7.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int main()
+//{
+//	int arr[] = { 0,1,2,3,4 };
+//	int arr1[] = { 5,6,7,8,9 };
+//	int arr2[] = { 10,11,12,13,14 };
+//	int* par[] = { arr,arr1,arr2 };
+//	int i = 0;
+//	for (i = 0; i < 3;i++)
+//	{
+//		int j = 0;
+//		for (j = 0;j < 5;j++)
+//		{
+//			//printf("%d ",*(par[i]+j));
+//			printf("%d ", par[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+#include<stdio.h>
+int main()
+{
+	int i = -1;
+	printf("%u", i);
+	return 0;
+}
--- prasix/prasix/pra6.c
+++ prasix/prasix/pra6.c
@@ -139,61 +139,173 @@
 //}
 
 //将句子进行逆置，如 i like you 变成 you like i
-int examine(char* orig)
-{
-	int count = 0;
-	while (*orig != '\0')
-	{
-		if (*orig == ' ')
-		{
-			count++;
-		}
-		orig++;
-	}
-	return count;
-}
-void Inversion(char* original,char* piracy)
-{
-	int start = 0;
-	int i = 0;
-	int tmp = 0;
-	while (examine(original))
-	{
-		int len = strlen(original);
-		for (i = 0;i < len;i++)
-		{
-			if (*(original + i) == ' ')
-				start = i + 1;
-		}
-		int tmp = start-1;
-		while (*(original + start))
-		{
-			*piracy = *(original + start);
-			piracy++;
-			start++;
-		}
-		*piracy = ' ';
-		piracy++;
-		*(original + tmp) = '\0';
-		
-	}
-	if(examine(original) == 0)
-	{
-		while (*original != '\0')
-		{
-			*piracy = *original;
-			original++;
-			piracy++;
-		}
-	}
+//int examine(char* orig)
+//{
+//	int count = 0;
+//	while (*orig != '\0')
+//	{
+//		if (*orig == ' ')
+//		{
+//			count++;
+//		}
+//		orig++;
+//	}
+//	return count;
+//}
+//
+////将字符数组单词逆置
+//void Inversion(char* original,char* piracy)
+//{
+//	int start = 0;
+//	int i = 0;
+//	int tmp = 0;
+//	while (examine(original))//根据空格剩余进行单词逆置
+//	{
+//		int len = strlen(original);
+//		for (i = 0;i < len;i++)
+//		{
+//			if (*(original + i) == ' ')
+//				start = i + 1;
+//		}
+//		int tmp = start-1;
+//		while (*(original + start))
+//		{
+//			*piracy = *(original + start);
+//			piracy++;
+//			start++;
+//		}
+//		*piracy = ' ';
+//		piracy++;
+//		*(original + tmp) = '\0';
+//		
+//	}
+//
+//	//当空格为0时将第一个单词拷贝进去
+//	if(examine(original) == 0)
+//	{
+//		while (*original != '\0')
+//		{
+//			*piracy = *original;
+//			original++;
+//			piracy++;
+//		}
+//	}
+//
+//}
+//int main()
+//{
+//	char sent_or[100] = { 0 };
+//	gets(sent_or);
+//	char sent_pi[100] = { 0 };
+//	Inversion(sent_or, sent_pi);
+//	printf("%s", sent_pi);
+//	return 0;
+//}
 
-}
-int main()
-{
-	char sent_or[100] = { 0 };
-	gets(sent_or);
-	char sent_pi[100] = { 0 };
-	Inversion(sent_or, sent_pi);
-	printf("%s", sent_pi);
-	return 0;
-}
+//用字符串逆置的方法进行句子逆置
+//#include<stdio.h>
+//#include<string.h>
+//void Inversion(char* fch, char* ech)
+//{
+//	char tmp = 0;
+//	int i = 0;
+//	while (fch<ech)
+//	{
+//		tmp = *fch;
+//		*fch = *ech;
+//		*ech = tmp;
+//		fch++;
+//		ech--;
+//	}
+//}
+//int main()
+//{
+//	char sent[100] = {0};
+//	gets(sent);
+//	int len = strlen(sent)-1;
+//	Inversion(sent, sent + len);//整个的逆置
+//
+//	//单词的逆置
+//	char* start = sent;
+//	char* end = start;
+//	while (1)
+//	{
+//		end = start;
+//		while (*end != ' ' && *end != 0)
+//		{
+//			end++;
+//		}
+//		Inversion(start, end-1);
+//		if (*end == 0)
+//			break;
+//		else
+//			start = end + 1;
+//	}
+//	puts(sent);
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int i = -20;
+//	unsigned int j = 10;
+//	printf("%d\n", i+j);
+//	return 0;
+//}
+//int main()
+//{
+//	char arr[1000];
+//	int i = 0;
+//	for (i = 0;i < 1000;i++)
+//	{
+//		arr[i] = -1 - i;
+//	}
+//	printf("%d", strlen(arr));
+//}
+//int main()
+//{
+//	int a = 1;
+//	char* pa= &a;
+//	if (*pa == 1)
+//	{
+//		printf("是大端字节序大端存储\n");
+//	}
+//	else
+//		printf("是小端字节序存储\n");
+//	return 0;
+//}
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+//int* twoSum(int* nums, int numsSize, int target, int* returnSize)
+//{
+//    int start = 0;
+//    while(nums + start < nums + numsSize)
+//    {
+//
+//        int next = start + 1;
+//     while (nums + next != nums + numsSize)
+//     {
+//        if (nums[start] + nums[next] == target)
+//        {
+//          returnSize[0] = start;
+//          returnSize[1] = next;
+//          return returnSize;
+//        }
+//        else
+//        next++;
+//     }
+//     start++;
+//    }
+//}
+//int main()
+//{
+//    int nums[] = { 2,7,10,13 };
+//    int target = 9;
+//    int return_s[20] = { 0 };
+//    int nums_len = 4;
+//    twoSum(nums, nums_len, target, return_s);
+//    printf("%s", );
+//    return 0;
+//}
+//
