1c83e2ea602e0bd8353ec33d97f3cebed56a395e
lu-yousheng <2269975236@qq.com>
2023-04-07 15:31:31 +0800

    22222

--- pra_4-1/pra_4-1/pra.c
+++ pra_4-1/pra_4-1/pra.c
@@ -131,43 +131,181 @@
 //	printf("%s", arr);//bcda
 //	return 0;
 //}
+//#include<stdio.h>
+//#include<string.h>
+//void coso_arr(char* arr, int k)
+//{
+//	char* fit = arr;
+//	char tmp = 0;
+//	int sz = strlen(arr);
+//	if (k > 1)
+//	{
+//		while (fit <= (arr + (sz / k)))
+//		{
+//			tmp = *fit;
+//			*fit = *(fit + k);
+//			*(fit + k) = tmp;
+//			fit++;
+//		}
+//	}
+//	else
+//	{
+//		while (fit < (arr+sz-1))
+//		{
+//			tmp = *fit;
+//			*fit = *(fit + k);
+//			*(fit + k) = tmp;
+//			fit++;
+//		}
+//
+//	}
+//
+//}
+//int main(void)
+//{
+//	char arr[20] = "ABCD";
+//	scanf("%s", arr);
+//	int input = 1;
+//	scanf("%d", &input);
+//	coso_arr(arr, input);
+//	printf("%s", arr);
+//	return 0;
+//}
+//左旋字符
+//#include<stdio.h>
+//void revolve(char* arr, int k)
+//{
+//	int sl = strlen(arr);
+//	char tmp = 0;
+//	int i = 0;
+//
+//	//字符放置到后面的次数
+//	for (i = 0;i < k;i++)
+//	{
+//		int j = 0;
+//		tmp = arr[0];
+//		for (j = 0;j <sl-1 ;j++)
+//		{
+//			arr[j] = arr[j + 1];
+//		}
+//		arr[j] = tmp;
+//	}
+//}
+//int main(void)
+//{
+//	char arr[20] = "ABCD";
+//	int input = 1;
+//	scanf("%s", arr);
+//	scanf("%d", &input);
+//
+//	revolve(arr, input);
+//	printf("%s",arr);
+//	return 0;
+//}
+//三步旋转法
+//AB CDEF
+//BA FEDC
+//CDEF BA
+//abcd-adcb-bcda
+//#include<stdio.h>
+//void revolve(char* fit, char* lat)
+//{
+//	char tmp = 0;
+//	while (fit<lat)
+//	{
+//		tmp = *fit;
+//		*fit = *lat;
+//		*lat = tmp;
+//		fit++;
+//		lat--;
+//	}
+//}
+//int main(void)
+//{
+//	char arr[20] = "ABCDE";
+//	int input = 2;
+//	int sl = strlen(arr);
+//	scanf("%s", arr);
+//	scanf("%d", &input);
+//	revolve(arr, arr + input-1);
+//	revolve(arr + input, arr + sl - 1);
+//	revolve(arr, arr + sl - 1);
+//	printf("%s", arr);
+//	return 0;
+//}
+//写一个函数，判断这个字符串是否为另一个字符串的逆置，
+//例如，给定一个s1=AABCD和s2=BCDAA返回1；
+//给定s1=abcd和s2=ACBD，返回0；
 #include<stdio.h>
-#include<string.h>
-void coso_arr(char* arr, int k)
+void revolve(char* arr, int k)
 {
-	char* fit = arr;
+	int sl = strlen(arr);
 	char tmp = 0;
-	int sz = strlen(arr);
-	if (k > 1)
+	int i = 0;
+
+	//字符放置到后面的次数
+	for (i = 0;i < k;i++)
 	{
-		while (fit <= (arr + (sz / k)))
+		int j = 0;
+		tmp = arr[0];
+		for (j = 0;j < sl - 1;j++)
 		{
-			tmp = *fit;
-			*fit = *(fit + k);
-			*(fit + k) = tmp;
-			fit++;
+			arr[j] = arr[j + 1];
 		}
+		arr[j] = tmp;
+	}
+}
+
+int judge_rel(char* arr, char* arr1)
+{//AABCD BCDAA
+	int count = 0;
+	char* fit = arr;
+	char* fit1 = arr1;
+	int sl = strlen(arr);
+	int sl1 = strlen(arr1);
+
+	if (sl != sl1)
+	{
+		return 0;
+	}
+	while (sl)
+	{
+		if (*arr==*arr1)
+		{
+			count++;
+			arr++;
+			arr1++;
+		}
+		else
+		{
+			arr++;
+		}
+		sl--;
+	}
+	int rel = sl1 - count;
+	revolve(fit, rel);
+	if (strcmp(fit, fit1) == 0)
+	{
+		return 1;
 	}
 	else
 	{
-		while (fit < (arr+sz-1))
-		{
-			tmp = *fit;
-			*fit = *(fit + k);
-			*(fit + k) = tmp;
-			fit++;
-		}
-
+		return 0;
 	}
-
+	return 0;
 }
-int main(void)
+int main()
 {
-	char arr[20] = "ABCD";
+	char arr[20] = "AABCD";
+	char arr1[20] = "BCDAA";//da abc
 	scanf("%s", arr);
-	int input = 1;
-	scanf("%d", &input);
-	coso_arr(arr, input);
-	printf("%s", arr);
+	scanf("%s", arr1);
+	int ret = judge_rel(arr, arr1);
+	if (ret == 1)
+	{
+		printf("是");
+	}
+	else
+		printf("不是");
 	return 0;
 }
